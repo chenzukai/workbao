26ddb7a987c4189ab8996a73dd31600f4eadf0e3
chenzukai <31817174+chenzukai@users.noreply.github.com>
2025-11-20 17:35:58 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'jin10-pwa-v1';
+const CACHE_NAME = 'jin10-news-v1';
 const urlsToCache = [
   './',
-  './index.html'
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -12,10 +13,6 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // 对于 API 请求，总是网络优先；对于静态资源，缓存优先
-  if (event.request.url.includes('/api/')) {
-      return; 
-  }
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
